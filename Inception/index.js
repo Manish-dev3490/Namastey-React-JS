12bfef9784c2e41c995864e55d039168b5d0b287
manish-dev3490 <kmanish87064@gmail.com>
2024-03-07 18:03:00 +0530

    done

--- Inception/index.js
+++ Inception/index.js
@@ -8,8 +8,8 @@ getRoot.render(makeHeading);
 
 
 // now making the second project which considered nested divs like this <div><h2></h2></div>
-
 const makeNestedElements = React.createElement("div", { class: "firstDiv" }, [React.createElement("h2", {}, "Hello world "),
 React.createElement("h2", {}, "Hello world 2")]);
-
 getRoot.render(makeNestedElements);
+
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Namastey-React-JS
+# In this file i will push my codes and notes in order with my learning
