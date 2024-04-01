dbab867e0589dcba228a585f61088a2548eb518e
manish-dev3490 <kmanish87064@gmail.com>
2024-04-01 17:57:38 +0530

    undestand functional based component

--- Laying-Foundation/index.js
+++ Laying-Foundation/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
+// writing JSX
 const root =ReactDOM.createRoot(document.querySelector(".container"));
 const firstHeading=<h1 className="heading"> Hello I am writing JSX</h1>;
 
 root.render(firstHeading);
+
+// understanding function based commponents
