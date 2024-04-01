90359a32a1e7b25b46b5d9865ff08428e2ee4b2c
manish-dev3490 <kmanish87064@gmail.com>
2024-04-01 18:12:34 +0530

    done functional  based component

--- Laying-Foundation/index.js
+++ Laying-Foundation/index.js
@@ -1,10 +1,23 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
+
+
+const root = ReactDOM.createRoot(document.querySelector(".container"));
+
 
 // writing JSX
-const root =ReactDOM.createRoot(document.querySelector(".container"));
-const firstHeading=<h1 className="heading"> Hello I am writing JSX</h1>;
+const firstHeading = <h1 className="heading"> Hello I am writing JSX</h1>;
+const secondHeading = <h1 className="heading"> Hello I am writing JSX again</h1>;
 
-root.render(firstHeading);
 
 // understanding function based commponents
+const Makeheading = function () {
+    return firstHeading;
+}
+
+
+const Makesubheading = () => {
+    return secondHeading;
+}
+root.render(<Makeheading/>);
+
