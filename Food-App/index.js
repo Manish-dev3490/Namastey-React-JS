563c3161ef17feb34c0691b084c1f35292fb46ef
manish-dev3490 <kmanish87064@gmail.com>
2024-04-02 15:34:14 +0530

    header part of react and css is  done

--- Food-App/index.js
+++ Food-App/index.js
@@ -7,11 +7,39 @@ import ReactDOM from "react-dom/client";
 // this is the main root which fetched from html file
 const rootElement = ReactDOM.createRoot(document.querySelector(".root"));
 
+
+
+// lets make the header commponent
+const Headercomponent=()=>{
+    return (
+        <header className="header">
+            <div className="logo-box">
+                
+                <h2>Food Paradise</h2>
+            </div>
+
+        
+            <ul className="nav-items">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+
+            </ul>
+            
+        </header>
+    )
+}
+
+
+
 // Lets make a Main Application component in which all component will include
 const MainApplicationComponent = () => {
 return (
     <div className="container">
-        
+        <Headercomponent/>
     </div>
 )
 }
+
+rootElement.render(<MainApplicationComponent/>);
