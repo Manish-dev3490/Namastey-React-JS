4d9b3e6a57847b7ab9096bdef751df3ec50cd943
manish-dev3490 <kmanish87064@gmail.com>
2024-04-02 15:09:19 +0530

    main application component initial is done

--- Food-App/index.js
+++ Food-App/index.js
@@ -1,5 +1,17 @@
 import React from "react";
-import  ReactDOM   from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
 // Now we are going to make the basic food app for practicing the components props and some other react tools for making our foundation strong 
 
+
+// this is the main root which fetched from html file
+const rootElement = ReactDOM.createRoot(document.querySelector(".root"));
+
+// Lets make a Main Application component in which all component will include
+const MainApplicationComponent = () => {
+return (
+    <div className="container">
+        
+    </div>
+)
+}
